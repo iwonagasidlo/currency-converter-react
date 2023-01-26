c49b5435ca81b9c2dffb97215c08e85a05bfe877
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-01-26 10:15:03 +0100

    Updates

---
+++ precache-manifest.6ed6f8336fed37c96da951260b0a989d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7871b9713b42b7b9061",
+    "url": "/currency-converter-react/static/css/main.46541ffc.chunk.css"
+  },
+  {
+    "revision": "b7871b9713b42b7b9061",
+    "url": "/currency-converter-react/static/js/main.b7871b97.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/currency-converter-react/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/currency-converter-react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "836df5ce65397d144d0f",
+    "url": "/currency-converter-react/static/js/runtime~main.836df5ce.js"
+  },
+  {
+    "revision": "09fbdd8c5d8bdbc16651504dfb85dc93",
+    "url": "/currency-converter-react/index.html"
+  }
+];
