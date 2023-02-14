4312c3ade9fd8da164046dd4b7ae186458722d5a
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-02-14 16:05:44 +0100

    theme folder with colors and breakpoint created

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        black: "#000",
+        chalice: "#aaa",
+        white: "#fff",
+        alto: "#D6D6D6",
+        teal: "#008080",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    }
+}
