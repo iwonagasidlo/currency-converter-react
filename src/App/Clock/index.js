0eff98e7abd7cad5a373a991bd132ddd07b731eb
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-02-17 09:13:00 +0100

    custom hook useCurrentDate created

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import "./style.css";
+import React from "react";
+import { DateAndTime } from "./styled";
+import { useCurrentDate } from "./useCurrentDate";
 
 const formatDate = (date) => date.toLocaleString(undefined, {
     weekday: "long",
@@ -11,22 +12,12 @@ const formatDate = (date) => date.toLocaleString(undefined, {
 });
 
 export const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    const date = useCurrentDate();
 
     return (
-        <div className="clock">
+        <DateAndTime>
             Dzisiaj jest {" "}
             {formatDate(date)}
-        </div>
+        </DateAndTime>
     )
 };          
