014973e19168492299b1d51cefbd991927ed1159
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-02-17 09:18:28 +0100

    styled.js created according to styled-components

---
+++ src/App/Clock/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DateAndTime = styled.div`
+    font-family: monospace;
+    color: ${({ theme }) => theme.color.chalice};
+    text-align: right;
+`;
