6d3429e5e55e363a2df172992d0c1ef787ae7cf6
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2023-02-25 18:57:56 +0100

    calculateResult and setResult moved to form index.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,34 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { Form } from './Form';
 import { Clock } from './Clock';
-import { currencies } from './currencies';
-import { hello } from "../utils/hello";
 import { Wrapper } from "./styled";
+import { hello } from "../utils/hello";
 
 hello();
 
 function App() {
-    const [result, setResult] = useState();
-
-    const calculateResult = (currency, amount) => {
-        const rate = currencies
-            .find(({ short }) => short === currency)
-            .rate;
-
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-        });
-    }
 
     return (
         <Wrapper>
             <Clock />
-            <Form
-                result={result}
-                calculateResult={calculateResult}
-            />
+            <Form />
         </Wrapper>
     );
 }
